476b935dd14b599efd629cc0a0185bcbc8019ef3
Viktor Zherdev <vrzherdev@gmail.com>
2014-02-02 23:15:37 +0200

    список JSON
    Signed-off-by: Viktor Zherdev <vrzherdev@gmail.com>

--- myapp/routes/user.js
+++ myapp/routes/user.js
@@ -4,5 +4,20 @@
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+  res.json('users', {    
+   
+"Students": [
+
+{ "Name":"Amit Goenka" ,
+"Major":"Physics" },
+{ "Name":"Smita Pallod" ,
+"Major":"Chemistry" },
+{ "Name":"Rajeev Sen" ,
+"Major":"Mathematics" }
+]
+}
+    
+
+); 
 };
+
