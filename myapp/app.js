bae6ead18454b012f6db6ae2b6f0fc4c0b2dcd60
Viktor Zherdev <vrzherdev@gmail.com>
2014-02-03 09:30:34 +0200

    изменил путь к юзер
    Signed-off-by: Viktor Zherdev <vrzherdev@gmail.com>

--- myapp/app.js
+++ myapp/app.js
@@ -5,7 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+var user = require('./routes/list/user');
 var http = require('http');
 var path = require('path');
 
@@ -32,7 +32,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/users/list', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
